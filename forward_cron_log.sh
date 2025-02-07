59253c230a25f147d5375cfbc176e11e70164ae8
SaltySOMAdmin <157048676+SaltySOMAdmin@users.noreply.github.com>
2025-02-07 14:09:55 -0500

    Update forward_cron_log.sh

--- forward_cron_log.sh
+++ forward_cron_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-LOG_FILE="/home/ubuntu/Reddit-UFOs_Archive/cron_log.txt"
+LOG_FILE="/home/ubuntu/Reddit-UFOs_Archive/cron_log.txt" # log location
 source "/home/ubuntu/Reddit-UFOs_Archive/cron_webhook.txt"  # Load webhook URL or file containing it
 
 # Check if the log file exists
