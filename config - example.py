7987a64f55e0139753eece1922a12ebcdbc3f9b9
SaltySOMAdmin <157048676+SaltySOMAdmin@users.noreply.github.com>
2025-03-07 10:59:13 -0500

    Update config - example.py

--- config - example.py
+++ config - example.py
@@ -1,4 +1,4 @@
-# rename to config.py, enter your credentials, and save to the same folder as your script
+# Rename this file to config.py. Enter your credentials and save to the same folder as your script. If you're using a public repo then ensure this file is in your .gitignore list! 
 
 # SourceAccount
 source_client_id=""
