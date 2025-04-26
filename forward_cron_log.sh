8ba3404e04d9ded68a2797591e1339408cafcc89
SaltySOMAdmin <157048676+SaltySOMAdmin@users.noreply.github.com>
2025-04-26 18:06:47 -0400

    Update forward_cron_log.sh

--- forward_cron_log.sh
+++ forward_cron_log.sh
@@ -4,12 +4,6 @@
 LOG_FILE="/home/ubuntu/Reddit-UFOs_Archive/cron_log.txt" # log location specified in cron
 source "/home/ubuntu/Reddit-UFOs_Archive/cron_webhook.txt"  # Load webhook URL or file containing it
 
-# Check if the log file exists
-if [ ! -f "$LOG_FILE" ]; then
-    echo "Log file does not exist: $LOG_FILE"
-    exit 1
-fi
-
 # Check if the log file is empty
 if [ ! -s "$LOG_FILE" ]; then
     echo "Log file is empty. Exiting."
