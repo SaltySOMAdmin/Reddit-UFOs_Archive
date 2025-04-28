09ee312862219352e7793f9b2f12fd3a1c4df366
SaltySOMAdmin <157048676+SaltySOMAdmin@users.noreply.github.com>
2025-04-28 13:17:17 -0400

    Update CopyPosts-UFOs_Archives.py

--- CopyPosts-UFOs_Archives.py
+++ CopyPosts-UFOs_Archives.py
@@ -174,7 +174,11 @@ for submission in source_subreddit.new():
             if submission.selftext:
                 comment_body += f"\n\nOriginal post text: {submission.selftext}"
                 comment_body += f"\n\n**Original Post ID:** `{submission.id}`"
-                comment_body += f"\n\n**Original Post Flair ID:** `{submission.link_flair_template_id}`"
+                # Add flair ID if available
+            if submission.link_flair_template_id:
+                comment_body += f"**Original Flair ID:** `{submission.link_flair_template_id}`\n"
+            if submission.link_flair_text:
+                comment_body += f"**Original Flair Text:** `{submission.link_flair_text}`"
 
             if len(comment_body) > 10000:
                 for chunk in split_text(comment_body):
